4d41d2ad5485fbd982a9d234cba9d6d09c906ef8
hungrybeagle <cyang@deltalearns.ca>
2022-10-16 20:26:21 -0700

    Update example1.py

--- example1.py
+++ example1.py
@@ -1,8 +1,8 @@
 #!python
 
 '''
-Using the "in" operator to search tuples, lists and dicts
-This can tell you if a particular value can be found in a list or tuple, but it can only find keys in dictionaries
+Using the "in" operator to search tuples, lists, strings and dicts
+This can tell you if a particular value can be found in a list, tuple or string, but it can only find keys in dictionaries
 '''
 
 a = (3,4,5,6,7)
@@ -14,6 +14,7 @@ c = {
     "d" : 6,
     "e" : 7
 }
+d = "34567"
 
 if 3 in a:
     print("found in tuple")
@@ -23,4 +24,6 @@ if 3 in c:
     print("value found in dict")
 if "c" in c:
     print("key found in dict")
+if "3" in d:
+    print("character found in string")
 
