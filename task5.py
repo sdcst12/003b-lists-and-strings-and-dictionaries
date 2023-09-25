9574be6afd69cad0fe990e23b526faf568cb429d
Chris Yang <cyang@deltalearns.ca>
2023-09-24 21:02:24 -0700

    update for 2024

--- task5.py
+++ task5.py
@@ -1,13 +1,13 @@
 #!python3
 """
-##### Task 3
+##### Task 5
 create a dictionary for an inventory of items in a game.  Ask the user for input, and if the item they choose to 'get item', add that item to their inventory.  If they choose to drop item' remove that item from that invenory.  If they choose 'show inventory' give them a list of the items that they have.
 
 Possible extensions:
 * nicer format for displaying inventory
 * use shortened/abbreviated names for items (recognizing first few characters or spelling errors)
 
-items in inventory:
+possible items:
 food
 water
 rope
@@ -27,10 +27,12 @@ example:
 >get water
 >get water
 >get iron
+>get 3 wood
 >inventory
 You have:
  1 food
  2 water
  1 iron
+ 3 wood
  >
 """
