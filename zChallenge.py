9d944f06e40b71d1eae4beeb2e81492979ad8d1f
yuriif671 <yuriif767@deltalearns.ca>
2024-11-02 23:19:13 -0700

    Done

--- zChallenge.py
+++ zChallenge.py
@@ -12,3 +12,12 @@ for i in range(40):
 print(x)
 
 
+"""
+imports the 'random' module
+creates a list 'x'
+loops 40 times
+selects either 0 or 1
+if 1 then add an integer from 1 to 10 to the list 'x'
+if 0 then add a decimal from 1.0 to 10.9 to the list 'x'
+print list 'x'
+"""
